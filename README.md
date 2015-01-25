cd797c9840f563cd724b79785f5f1a743e861ac5
阿狼 <stormte@gmail.com>
2015-01-25 21:53:51 +0800

    1.0.12

--- README.md
+++ README.md
@@ -1,4 +1,42 @@
 viewtorem
 =========
 
-css文件中的width、height值如果以px为单位，将会被转化成rem单位
+替换css属性值中的px
+
+###安装
+
+    npm install -g viewtorem
+
+###初始化
+
+    viewtorem init
+
+初始化后会在当前目录生成一个名为`viewtorem.json`的配置文件，如下：
+
+```javascript
+{
+    //待处理文件值可以是数组或字符串
+    //例如：“./style”，当前目录下style文件夹下所有css文件(会自动排除包含‘-rem.css’这些已经转换过的样式表)
+    "files": ["style.css"],
+    //默认根目录字体大小(px)
+    "root_value": 16,
+    //保留小数位（最后一位四舍五入）
+    "unit_precision": 5,
+    //需要换算的属性
+    //"font", "font-size", "line-height", "letter-spacing"
+    "prop_white_list": ["width", "height", "padding", "padding-top", "padding-right", "padding-bottom", "padding-left", "margin", "margin-top", "margin-right", "margin-bottom", "margin-left"],
+    //布尔值，是否替换掉属性值
+    //默认会追加
+    "replace": false,
+    //布尔值，是否替换media screen中的属性值
+    //例如“@media screen and (max-width:240px)”
+    "media_query": false
+}
+```
+
+###执行
+
+    viewtorem build
+
+
+
--- index.js
+++ index.js
@@ -1,84 +1,88 @@
 #!/usr/bin/env node
 
-var config = require("./config.json"),
-	fs = require("fs"),
-	path = require("path"),
-	buffer = require("buffer").Buffer;
-var rootValue = 20;
-var unitPrecision = 5;
-//Buffer则node中储存二进制数据的中介者
-console.log("viewtorem is at :" + __dirname + "\r\njust rewrite it as you wish\r\n");
-//取到参数
+var fs = require("fs"),
+    stat = fs.stat,
+    util = require('util'),
+    path = require('path'),
+    buffer = require("buffer").Buffer,
+    postcss = require('postcss'),
+    viewtorem = require('./lib/viewtorem.js');
+
+console.log("viewtorem is at :" + __dirname + "\r\njust rewrite it as you wish\r");
+
+//获取到参数
 var targetList = process.argv;
 if (targetList[0] == "node") {
-	targetList.shift();
+    targetList.shift();
 }
 targetList.shift();
 
 if (targetList.length <= 0) {
-	console.log("useage : viewtorem fileA [fileB [fileC]..]");
-	console.log("useage : viewtorem dirName");
-	process.exit(0);
+    console.log("useage : viewtorem init or 'viewtorem build'");
+    process.exit(0);
 }
-//fs.statSync同步获取文件信息
+
+var sourceJson = path.join(__dirname,"viewtorem.json");
+var targetJson = path.join(process.cwd(),"viewtorem.json");
+//处理参数
 for (var i = 0; i < targetList.length; i++) {
-	var target = targetList[i],
-		stat = fs.statSync(target);
-	if (stat.isFile()) {
-		mathcRuleForFile(target);
-	}
-	//fs.readdirSync读取目录的所有文件
-	if (stat.isDirectory()) {
-		var list = fs.readdirSync(target);
-		list.forEach(function(item) {
-			mathcRuleForFile(path.join(target, item));
-		});
-	}
+    var target = targetList[i];
+    if (target == "init") {
+        exists(sourceJson, targetJson, function(src, target) {
+            //
+        })
+    } else if (target == "build") {
+        exists(sourceJson, targetJson, function(src, target) {
+            var options = require(target);
+            var files = options.files;
+            if (util.isArray(files)) {
+                for (j = 0; j < files.length; j++) {
+                    dealfile(files[j], options);
+                }
+            } else {
+                var stat = fs.statSync(files);
+                if (stat.isDirectory()){
+                    var list = fs.readdirSync(files);
+                    list.forEach(function(item) {
+                        if (/\.css/.test(item)&&!/-rem\.css/.test(item)){
+                            dealfile(path.join(files, item), options);
+                        }
+                    });
+                }
+            }
+        })
+    }
 }
 
-function mathcRuleForFile(filePath) {
 
-	//traverse rules
-	for (var i = 0; i < config.length; i++) {
-		var configItem = config[i];
-
-		var reg = new RegExp(config[i].tail);
-		if (reg.test(filePath)) {
-			dealFile(filePath);
-			break;
-		}
-	}
-
-	function dealFile(filePath) {
-
-		if (!fs.existsSync(filePath)) {
-			console.log("file not exist :" + filePath);
-		} else {
-			fs.readFile(filePath, {encoding:"utf-8"}, function(err, data) {
-				if (err) throw err;
-
-				var pxRegexWidth = /width\s*:\s*(\d*\.?\d+)px/ig;
-				var pxReplaceWidth = function (m, $1) {
-				    return 'width:' + toFixed((parseFloat($1) / rootValue), unitPrecision) + 'rem';
-				};
-				var pxRegexHeight = /([^-]{1})height\s*:\s*(\d*\.?\d+)px/ig;
-				var pxReplaceHeight = function (m, $1, $2) {
-				    return $1 + 'height:' + toFixed((parseFloat($2) / rootValue), unitPrecision) + 'rem';
-				};
-				//var aa = fs.readFileSync(filePath, { encoding: "utf8" });
-				var finalResult = data.replace(pxRegexWidth, pxReplaceWidth).replace(pxRegexHeight, pxReplaceHeight);
-
-				process.stdout.write("--> " + filePath + "...");
-				//var finalResult = buffer.concat([currentComment, data]);
-				//fs.unlinkSync(filePath);
-				fs.writeFileSync(filePath.split(".")[0]+'-rem.css', finalResult)
-				process.stdout.write("done\r\n");
-			});
-		}
-	}
+//检测文件是否存在
+function exists(src, target, callback) {
+    fs.exists(target, function(exists) {
+        if (exists) {
+            callback(src, target);
+        } else {
+            fs.readFile(src, function(err,data){
+                if (err) throw err;
+                fs.writeFileSync(target, data);
+                callback(src, target);
+            })
+        }
+    })
 }
-function toFixed(number, precision) {
-    var multiplier = Math.pow(10, precision + 1),
-    wholeNumber = Math.floor(number * multiplier);
-    return Math.round(wholeNumber / 10) * 10 / multiplier;
+
+//处理文件
+function dealfile(filename, options) {
+    if (!fs.existsSync(filename)){
+        console.log("css file does not exist");
+        return
+    }
+    var css = fs.readFileSync(filename, {encoding:"utf8"});
+    var processedCss = postcss(viewtorem(options)).process(css).css;
+    process.stdout.write("--> " + filename + "...");
+    fs.writeFile(path.normalize(filename).split(".")[0]+'-rem.css', processedCss, function(err) {
+        if (err) {
+            throw err;
+        }
+        process.stdout.write("done\r\n");
+    });
 }
